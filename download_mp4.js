2fb7c70003723b65dfb019f90056a7a5a417da3c
yangyuan <yangyuan@192.168.1.7>
2018-02-05 23:54:16 +0800

    爬取视频完成

--- download_mp4.js
+++ download_mp4.js
@@ -2,6 +2,10 @@ var request = require("request")
 
 var fs = require("fs")
 
+var path = process.argv.splice(2).join("").replace(/\s+/g, "") //命令行传入路径参数
+
+var videosInfo = JSON.parse(fs.readFileSync(`coures_categorys/${path}/download_url.txt`,'utf-8'))//读取视频列表
+
 var Bagpipe = require('bagpipe');
 
 var bagpipe = new Bagpipe(10);
@@ -23,24 +27,13 @@ var downloadFile = function(url, filename, callback) {
         var totalSize = res.headers['content-length']
         res.on("data", data => {
             downloadSize += data.length
-            pb.render({ completed: downloadSize, total: totalSize });
+            // pb.render({ completed: downloadSize, total: totalSize });
         })
     }).pipe(stream).on('close', callback)
 }
 
-var videos = [
-    {
-        name: '第1讲 集合（一）',
-        url: 'http://yx-kbs-ks3.haofenshu.com/videos/43d65926ecf90929950c9c80311ee094.mp4'
-    },
-    {
-        name: '第2讲 集合（二）',
-        url: 'http://yx-kbs-ks3.haofenshu.com/videos/7880f719be68f57c1e871f8a9592364f.mp4'
-    }
-]
-
-for(var i = 0; i < videos.length; i++){
-    bagpipe.push(downloadFile,videos[i].url, 'videos/' + videos[i].name.replace(/\s+/g, "") + '.mp4', function () {
+for(var i = 0; i < videosInfo.length; i++){
+    bagpipe.push(downloadFile,videosInfo[i].downloadUrl, `coures_categorys/${path}/${videosInfo[i].name.replace(/\s+/g, "")}.mp4`, function () {
     
     })
 }
